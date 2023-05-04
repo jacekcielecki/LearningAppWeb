407cf2b2ddefdc274a4463222ba1f92b7dec5e6b
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-05-04 17:44:32 +0200

    add react mui

--- src/CreateAccount.tsx
+++ src/CreateAccount.tsx
@@ -1,6 +1,15 @@
+import React from "react";
+import Button from '@mui/material/Button';
+import { TextField } from "@mui/material";
+import App from "./App";
+
 export const CreateAccount = () => {
     return (
-        <h3>Create Account</h3>
+        <div>
+            <h3>Create Account</h3>
+            <Button variant="contained">Hello World</Button>
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
+        </div>
     );
 }
  
