730e1f777cc67e8603ad1fb0f4ea64eb0eea529b
Jacek Cielecki <jcielecki@sii.pl>
2023-12-28 13:03:36 +0100

    Add footer style

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -2,13 +2,13 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 export default function Footer(){
-    return(
-        <footer>
-            <Box sx={{ display: 'flex', bgcolor: 'var(--lap-grey)', height: '30px'}}>
-                    <Typography variant="body1" noWrap component="div">
-                    LearningApp 2023
-                    </Typography>
-            </Box>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <Box sx={{ display: 'flex', alignItems: 'center', height: '40px', borderTop: 'solid var(--lap-grey) 1px', backgroundColor: 'var(--lap-white)' }}>
+        <Typography variant="body1" noWrap component="div" style={{ paddingLeft: 20 }}>
+          © LearningApp {new Date().getFullYear()}
+        </Typography>
+      </Box>
+    </footer>
+  );
+};
