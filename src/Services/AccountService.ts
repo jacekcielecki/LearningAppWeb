aa09239de6f6452ce06380464d54e5da0085aa1f
Jacek Cielecki <jcielecki@sii.pl>
2024-01-03 12:10:59 +0100

    Fix login

--- src/Services/AccountService.ts
+++ src/Services/AccountService.ts
@@ -4,13 +4,25 @@ import config from "../config";
 import Http from "./Http";
 
 export async function login(body: LoginDto) : Promise<boolean> {
-    const http = new Http();
-    const accessToken = await http.postAndGet<LoginDto, string>(`${config.apiUrl}/Account/login`, body);
-    if (accessToken !== "") {
-        localStorage.setItem("token", accessToken);
-        return true;
+    try {
+        const response = await fetch(`${config.apiUrl}/Account/login`,  {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(body),
+        });
+        if (!response.ok) {
+            throw new Error(`Error to POST data from ${`${config.apiUrl}/Account/login`} with statusCode ${response.status}`);
+        }
+        const accessToken: string = await response.text();
+        if (accessToken !== "") {
+            localStorage.setItem("token", accessToken);
+            return true;
+        }
+        return false;
+    } catch (error : any) {
+        console.error(error);
+        throw error;
     }
-    return false;
 }
 
 export async function register(body: CreateUserRequest): Promise<boolean> {
