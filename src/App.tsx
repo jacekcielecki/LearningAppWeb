00e312659bfda1d4e8f1f97489446c4866f623b0
Jacek Cielecki <jcielecki@sii.pl>
2024-01-08 14:35:53 +0100

    UserContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,45 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
 import { Home } from './Pages/Home/Home';
 import { Login } from './Pages/Login/Login';
 import { Dashboard } from './Pages/Dashboard/Dashboard';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import { jwtDecode } from "jwt-decode";
 import { getUser } from './Services/UserService';
 
+export const UserContext = createContext<string>("");
+
 function App() {
-  const [user, setUser] = useState<number | null>(null);
+  const [userContext, setUserContext] = useState<string>("");
 
-  const navigateToLandingPage = () => { 
-    // const location = useLocation();
-    // const currentPath = location.pathname;
-    // if(currentPath !== '/'){
-    //   const navigate = useNavigate(); 
-    //   navigate('/');
-    // }
-  }
-
-  const setState = async () => {
+  const setContexts = async () => {
     const token = localStorage.getItem('token');
-    if(token == null || token == ''){
-      navigateToLandingPage();
-    }
-    else {
+    if(token != null && token != ''){
       const decodedToken = jwtDecode(token);
-      if(decodedToken.jti == null){
-        navigateToLandingPage();
-      }
-      else {
-        const userId : number = parseInt(decodedToken.jti);
-        console.log(`User id: ${userId}`)
-
-        const user = await getUser(userId);
-        setUser(userId);
-        console.log(user);
+      if(decodedToken.jti != null){
+        const user = await getUser(parseInt(decodedToken.jti));
+        setUserContext(user.emailAddress);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    setState();
+    setContexts();
   }, []);
 
   return (
-    <Router>
-      <Layout>
-        <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/dashboard' element={<Dashboard />}/>
-        </Routes>
-      </Layout>
+    <UserContext.Provider value={userContext}>
+      <Router>
+        <Layout>
+          <Routes>
+              <Route path='/' element={<Home />}/>
+              <Route path='/login' element={<Login />}/>
+              <Route path='/dashboard' element={<Dashboard />}/>
+          </Routes>
+        </Layout>
     </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -4,10 +4,13 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from '../../App';
 
 export default function Header() {
-
   let navigate = useNavigate(); 
+  const userEmail = useContext(UserContext);
+
   const navigateHome = () =>{ 
     navigate('/');
   }
@@ -16,9 +19,12 @@ export default function Header() {
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" sx={{bgcolor: 'var(--lap-navy-blue-darken)'}}>
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div" onClick={navigateHome} sx={{cursor: 'pointer'}}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="h6" noWrap component="div" onClick={navigateHome} sx={{cursor: 'pointer', marginRight: '5'}}>
             LearningApp
+          </Typography>
+          <Typography variant="body1" noWrap component="div" onClick={navigateHome} sx={{cursor: 'pointer'}}>
+            {userEmail}
           </Typography>
         </Toolbar>
       </AppBar>
