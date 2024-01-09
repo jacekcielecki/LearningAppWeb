0ff4461aa9873b8b87a5a4911765ca257b2f92eb
Jacek Cielecki <jcielecki@sii.pl>
2024-01-09 12:59:02 +0100

    Show user menu only on auth routes

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -3,14 +3,16 @@ import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import UserMenu from './UserMenu';
 import UserAvatar from './UserAvatar';
 import UserContext from '../../Contexts/UserContext';
+import { authRoutes } from '../../routes';
 
 export default function Header() {
   let navigate = useNavigate(); 
+  const location = useLocation();
   const user = useContext(UserContext);
 
   const navigateHome = () =>{ 
@@ -25,13 +27,17 @@ export default function Header() {
           <Typography variant="h6" noWrap component="div" onClick={navigateHome} sx={{cursor: 'pointer', marginRight: '5'}}>
             LearningApp
           </Typography>
-          <div style={{display: 'flex', alignItems: 'center'}}>
-            <Typography variant="body1" noWrap component="div" sx={{cursor: 'pointer'}}>
-              {user?.emailAddress}
-            </Typography>
-            <UserAvatar/>
-            <UserMenu/>
-          </div>
+          {authRoutes.includes(location.pathname) ? 
+          (
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <Typography variant="body1" noWrap component="div" sx={{cursor: 'pointer'}}>
+                {user?.emailAddress}
+              </Typography>
+              <UserAvatar/>
+              <UserMenu/>
+            </div>
+          ) : 
+          (<></>)}
         </Toolbar>
       </AppBar>
     </Box>
