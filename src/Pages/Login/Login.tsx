fcb6d3fd07fc95fe91566fe516fc8afa3282171a
Jacek Cielecki <jcielecki@sii.pl>
2024-01-19 16:24:44 +0100

    Login page add Loader, fix /register routing

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -3,19 +3,24 @@ import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LoginDto } from '../../Models/Account/LoginDto';
 import { CreateUserRequest } from '../../Models/Account/CreateUserRequest';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import AccountService from '../../Services/AccountService';
+import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
 
 export const Login = () => {
+    const location = useLocation();
     const navigate = useNavigate(); 
+    const [isLoading, setIsLoading] = useState(false);
+    const [isRegisterMode, setIsRegisterMode] = useState(false);
+
+
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [confirmPassword, setConfirmPassword] = useState<string>('');
     const [email, setEmail] = useState<string>('');
-    const [isRegisterMode, setIsRegisterMode] = useState<boolean>(false);
 
     const navigateToDashboard = () =>{ 
         navigate('/dashboard');
@@ -23,12 +28,7 @@ export const Login = () => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (isRegisterMode) {
-            await handleRegister(e);
-        }
-        else {
-            await handleLogin(e);
-        }
+        isRegisterMode ? await handleRegister(e) : await handleLogin(e);
     };
 
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -70,46 +70,65 @@ export const Login = () => {
     };
 
     const ToggleMode = () => {
-        setIsRegisterMode(!isRegisterMode);
+        location.pathname == '/register' ?
+            navigate('/login') :
+            navigate('/register');
     }
 
-    return (
-        <div className='signin'>
-            <form onSubmit={handleSubmit}>
-                <Container component="main" maxWidth="xs">
-                    {isRegisterMode ? (
-                            <>
-                            <Typography component="h1" variant="h5" sx={{ mt: 2, mb: 2 }}>
-                                Create new account
-                            </Typography>
-                            <TextField onChange={(e) => setUsername(e.target.value)} margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
-                            <TextField onChange={(e) => setEmail(e.target.value)} margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email"/>
-                            <TextField onChange={(e) => setPassword(e.target.value)} margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
-                            <TextField onChange={(e) => setConfirmPassword(e.target.value)} margin="normal" required fullWidth name="confirmPassword" label="Confirm password" type="password" id="confirmPassword" autoComplete="current-password" />
-                            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 4 }}>
-                                Sign In
-                            </Button>
-                        </>
-                    ) :
-                        (
-                            <>
-                                <Typography component="h1" variant="h5" sx={{ mt: 2, mb: 2 }}>
-                                    Sign in
-                                </Typography>
-                                <TextField onChange={(e) => setEmail(e.target.value)} margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
-                                <TextField onChange={(e) => setPassword(e.target.value)} margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
-                                <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 4 }}>
-                                    Sign In
-                                </Button>
-                                <Link variant="body2" onClick={ToggleMode}>
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </>
-                        )
-                    }
+    useEffect(() => {
+        location.pathname == '/register' ?
+            setIsRegisterMode(true) :
+            setIsRegisterMode(false);
+    }, [location.pathname]);
 
-                </Container>
-            </form>
-        </div>
+    return (
+        <>
+            {isLoading ? 
+            <>
+                <div className="loader-container">
+                    <div className="loader loader-top-50">
+                        <CircularProgress color="secondary" size={50} />
+                    </div>
+                </div>
+            </> : 
+            <>
+                <div className='signin'>
+                    <form onSubmit={handleSubmit}>
+                        <Container component="main" maxWidth="xs">
+                            {isRegisterMode ? (
+                                    <>
+                                    <Typography component="h1" variant="h5" sx={{ mt: 2, mb: 2 }}>
+                                        Create new account
+                                    </Typography>
+                                    <TextField onChange={(e) => setUsername(e.target.value)} margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
+                                    <TextField onChange={(e) => setEmail(e.target.value)} margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email"/>
+                                    <TextField onChange={(e) => setPassword(e.target.value)} margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+                                    <TextField onChange={(e) => setConfirmPassword(e.target.value)} margin="normal" required fullWidth name="confirmPassword" label="Confirm password" type="password" id="confirmPassword" autoComplete="current-password" />
+                                    <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 4 }}>
+                                        Sign In
+                                    </Button>
+                                </>
+                            ) :
+                                (
+                                    <>
+                                        <Typography component="h1" variant="h5" sx={{ mt: 2, mb: 2 }}>
+                                            Sign in
+                                        </Typography>
+                                        <TextField onChange={(e) => setEmail(e.target.value)} margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+                                        <TextField onChange={(e) => setPassword(e.target.value)} margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+                                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 4 }}>
+                                            Sign In
+                                        </Button>
+                                        <Link variant="body2" onClick={ToggleMode}>
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </>
+                                )
+                            }
+                        </Container>
+                    </form>
+                </div>
+            </>}
+        </>
     );
 }
