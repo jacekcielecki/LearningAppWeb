fc4ff89c043913bc8027b5be17cf3d2a92674750
Jacek Cielecki <jcielecki@sii.pl>
2024-01-22 14:55:11 +0100

    Rework AuthGuard, add server offline handling

--- src/Components/Guards/AuthGuard.tsx
+++ src/Components/Guards/AuthGuard.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { authRoutes } from '../../routes';
+import { UserDto } from '../../Models/User/UserDto';
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+import UserService from '../../Services/UserService';
 
 interface AuthGuardProps {
   children: React.ReactNode;
@@ -9,16 +12,50 @@ interface AuthGuardProps {
 const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [jwtPayload, setJwtPayload] = useState<JwtPayload | null>(null);
+  const [userContext, setUserContext] = useState<UserDto | null>(null);
+
+  const navigateToErrorPage = () => { 
+    navigate('/error');
+  }
+
+  const navigateToLoginPage = () => { 
+    navigate('/login');
+  }
+
+  const validateJwtToken = (jwtToken : string | null) => {
+    if(!!jwtToken){
+      const decodedToken = jwtDecode(jwtToken);
+      if(!!decodedToken.jti && !!decodedToken.exp){
+        const currentTime = Date.now() / 1000;
+        const tokenExpired = decodedToken.exp < currentTime;
+        setJwtPayload(decodedToken);
+        return !tokenExpired;
+      }
+      return false;
+    }
+    return false;
+  }
+
+  const refreshUserContext = async () => {
+    if (userContext === null && !!jwtPayload?.jti){
+      const user = await UserService.GetById(parseInt(jwtPayload.jti));
+      user !== null ? setUserContext(user) : navigateToErrorPage();
+    }
+  }
+
+  const authorize = async () => {
+    const token = localStorage.getItem('token');
+    const isValidToken = validateJwtToken(token);
+    isValidToken ? await refreshUserContext() : navigateToLoginPage();
+  }
 
   useEffect(() => {
     if(authRoutes.includes(location.pathname)){
       /* Block not logged in users from accessing components that require authentication */
-      const token = localStorage.getItem("token");
-      if (token === null || token === "") {
-        navigate('/login');
-      }
+      authorize();
     }
-  }, [location.pathname, navigate]);
+  }, [location.pathname, navigate, userContext]);
 
   return <>{children}</>;
 };
