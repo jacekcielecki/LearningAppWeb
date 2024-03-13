e22ab39183f9e3b621bd3bae36c5e5012445680d
Jacek Cielecki <jcielecki@sii.pl>
2024-03-13 15:49:48 +0100

    Quiz page add answers

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,41 +1,51 @@
-import styled from "@emotion/styled";
-import { Box, Grid } from "@mui/material";
-import grey from "@mui/material/colors/grey";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import grey from '@mui/material/colors/grey';
 
-const Answer = styled(Box)(() => ({
-    m: 1,
-    p: 1,
-    bgcolor: '#fff',
-    color: grey[800],
-    border: '1px solid',
-    borderColor: grey[300],
-    borderRadius: '8px',
-    fontSize: '0.875rem',
-    fontWeight: '700',
-    margin: '8px',
-    padding: '8px',
-    width: '500px'
+const Answer = styled(Paper)(({}) => ({
+  backgroundColor: '#fff',
+  padding: '18px',
+  textAlign: 'left',
+  color: grey[800],
+  overflow: 'hidden',
+  border: '1px solid',
+  borderColor: grey[300],
+  borderRadius: '8px',
+  fontSize: '1rem',
+  fontWeight: '700',
+  boxShadow: 'none'
 }));
 
-const Quiz = () => {
-    return ( 
-        <Box sx={{display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
-            <div className='quiz-content'>
-                <Grid container rowSpacing={1}>
-                    <Grid item xs={6}>
-                        <Answer>
-                            {"1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"}
-                        </Answer>                   
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Answer>
-                            {"2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"}
-                        </Answer>
-                    </Grid>
+export default function RowAndColumnSpacing() {
+  return (
+    <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '120px'}}>
+        <Box sx={{ width: '80%', maxWidth: '1400px', display: 'flex', padding: '12px' }}>
+            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6}>
+                    <Answer>
+                        1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    </Answer>
                 </Grid>
-            </div>
-        </Box> 
-    );
+                <Grid item xs={6}>
+                    <Answer>
+                        2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    </Answer>
+                </Grid>
+                <Grid item xs={6}>
+                    <Answer>
+                        3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    </Answer>
+                </Grid>
+                <Grid item xs={6}>
+                    <Answer>
+                        4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    </Answer>
+                </Grid>
+            </Grid>
+        </Box>
+    </Box>
+
+  );
 }
- 
-export default Quiz;
