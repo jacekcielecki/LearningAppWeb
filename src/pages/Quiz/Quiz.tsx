9761312f77a7f39261d1766730cb72ca29a21d0d
Jacek Cielecki <jcielecki@sii.pl>
2024-03-14 13:28:38 +0100

    Add buton to go to next question

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -7,7 +7,7 @@ import ProgressBar from './ProgressBar';
 import { useEffect, useState, FC } from 'react';
 import QuestionDto from '../../interfaces/Question/QuestionDto';
 import QuestionService from '../../services/QuestionService';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const Answer = styled(Paper)(({}) => ({
@@ -26,6 +26,8 @@ const Answer = styled(Paper)(({}) => ({
 
 const Quiz: FC = () => {
     const [questions, setQuestions] = useState<QuestionDto[] | null>(null);
+    const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+
     const { categoryId } = useParams();
     const { level } = useParams();
     const navigate = useNavigate();
@@ -42,6 +44,15 @@ const Quiz: FC = () => {
         }
     };
 
+    const nextQuestion = () => {
+        if (questions === null) return;
+        if (currentQuestion < questions.length - 1) {
+            setCurrentQuestion(currentQuestion + 1);
+        } else {
+            alert('Quiz finished');
+        }
+    };
+
     useEffect(() => {
         fetchQuestions();
     }, []);
@@ -50,44 +61,50 @@ const Quiz: FC = () => {
     <>
         {(questions && questions.length > 0) && (
             <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '120px' }}>
-            <Box sx={{ width: '80%', maxWidth: '1400px', display: 'flex', padding: '12px' }}>
-                <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Box sx={{ width: '80%', maxWidth: '1400px', display: 'flex', padding: '12px' }}>
+                    <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
 
-                    <Grid item xs={12}>
-                        <ProgressBar questionsCount={questions?.length} currentQuestion={1} />
-                    </Grid>
+                        <Grid item xs={12}>
+                            <ProgressBar questionsCount={questions?.length} currentQuestion={currentQuestion + 1} />
+                        </Grid>
 
-                    <Grid item xs={12}>
-                        <h2>Select the correct answer</h2>
-                        <Typography>{questions?.[0].questionContent}</Typography>
-                    </Grid>
+                        <Grid item xs={12}>
+                            <h2>Select the correct answer</h2>
+                            <Typography>{questions?.[currentQuestion].questionContent}</Typography>
+                        </Grid>
 
-                    <Grid item xs={6}>
-                        <Answer>
-                            <b>1. </b>
-                            {questions?.[0].a}
-                        </Answer>
+                        <Grid item xs={6}>
+                            <Answer>
+                                <b>1. </b>
+                                {questions?.[currentQuestion].a}
+                            </Answer>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Answer>
+                                <b>2. </b>
+                                {questions?.[currentQuestion].b}
+                            </Answer>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Answer>
+                                <b>3. </b>
+                                {questions?.[currentQuestion].c}
+                            </Answer>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Answer>
+                                <b>4. </b>
+                                {questions?.[currentQuestion].d}
+                            </Answer>
+                        </Grid>
+
+                        <Grid item xs={12} mt={6}>
+                            <Box sx={{display: 'flex', justifyContent: 'end'}}>
+                                <Button variant='contained' color='primary' disableElevation onClick={nextQuestion}>Next</Button>
+                            </Box>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Answer>
-                            <b>2. </b>
-                            {questions?.[0].b}
-                        </Answer>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Answer>
-                            <b>3. </b>
-                            {questions?.[0].c}
-                        </Answer>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Answer>
-                            <b>4. </b>
-                            {questions?.[0].d}
-                        </Answer>
-                    </Grid>
-                </Grid>
-            </Box>
+                </Box>
             </Box>
         )}
     </>
